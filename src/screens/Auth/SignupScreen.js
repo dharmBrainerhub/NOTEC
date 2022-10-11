b05275bb8deb1e535aa25cad0978556313360957
sagarbrainerhub <sagarp.brainerhub@gmail.com>
2022-10-11 19:45:58 +0530

    -

--- src/screens/Auth/SignupScreen.js
+++ src/screens/Auth/SignupScreen.js
@@ -1,56 +1,66 @@
-import {Dimensions, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import React from 'react';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
-import { InputBox, Title } from '../../components';
-import { theme } from '../../utils';
+import {InputBox, Title} from '../../components';
+import {theme} from '../../utils';
 const h = Dimensions.get('screen').height;
 const w = Dimensions.get('screen').width;
 const SignupScreen = () => {
   return (
-    <View style={{flex: 1, backgroundColor: '#fff'}}>
-      <View style={styles.firstView}>
-        <Title
-
+    <KeyboardAvoidingView
+      style={{flex: 1, backgroundColor: theme.colors.white}}>
+      <ScrollView style={{flex: 1, height: theme.SCREENHEIGHT}}>
+        <View style={styles.firstView}>
+          <Title
+            style={{
+              color: theme.colors.white,
+              fontSize: 23,
+              // zIndex: 111,
+              marginTop: theme.SCREENHEIGHT * 0.1,
+              left: -theme.SCREENWIDTH * 0.1,
+            }}
+            title={'Create Account'}
+          />
+        </View>
+        <View
           style={{
-            color: '#000',
-            fontSize: 23,
-            zIndex: 111,
-            marginTop:theme.SCREENHEIGHT*0.02,
-            left:-theme.SCREENWIDTH*0.1
-          }}
-          title={'Create Account'}
-        />
-      </View>
-      <View
-        style={{
-          width: '80%',
-          alignSelf: 'center',
-          marginTop: h * 0.4,
-        }}>
-        <InputBox style={styles.textInput} placeholder="Name" />
-        <InputBox style={styles.textInput} placeholder="Your Email" />
-        <InputBox style={styles.textInput} placeholder="Password" />
-      </View>
-      <View style={styles.lastView} />
-    </View>
+            width: '80%',
+            alignSelf: 'center',
+            marginTop: h * 0.4,
+          }}>
+          <InputBox style={styles.textInput} placeholder="Name" />
+          <InputBox style={styles.textInput} placeholder="Your Email" />
+          <InputBox style={styles.textInput} placeholder="Password" />
+        </View>
+        <View style={styles.lastView} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default SignupScreen;
 const styles = StyleSheet.create({
   firstView: {
-    height:theme.SCREENHEIGHT/3,
+    height: theme.SCREENHEIGHT / 3,
     backgroundColor: theme.colors.purpal,
     position: 'absolute',
-    width:'100%',
+    width: '100%',
     top: -h * 0.02,
     // left: -w * 0.1,
     zIndex: -111,
     justifyContent: 'center',
     alignItems: 'center',
-    borderBottomRightRadius:theme.SCREENHEIGHT*7,
-    borderBottomLeftRadius:theme.SCREENHEIGHT*2,
-    
+    borderBottomRightRadius: theme.SCREENHEIGHT * 7,
+    borderBottomLeftRadius: theme.SCREENHEIGHT * 2,
   },
   lastView: {
     height: '50%',
@@ -62,12 +72,12 @@ const styles = StyleSheet.create({
     right: -w * 0.5,
   },
   textInput: {
-    // borderRadius: 10,
+    borderRadius: 10,
     // backgroundColor: '#DDA0DD',
-    // elevation: 15,
+    elevation: 15,
     // marginTop: 20,
     // fontSize: 18,
-    // paddingLeft: 15,
+    paddingLeft: 15,
     // fontWeight: '600',
   },
 });
